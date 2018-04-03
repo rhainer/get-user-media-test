50496a032b109301afa4b36054ed82ac686cd03f
Rob Hainer <robert@tokbox.com>
2018-04-03 12:59:09 -0700

    port

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 8080;
 
 // viewed at http://localhost:8080
 app.use(express.static(__dirname + '/public'))
@@ -8,4 +9,4 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res) {
     });
 
-app.listen(8080);
+app.listen(port);
